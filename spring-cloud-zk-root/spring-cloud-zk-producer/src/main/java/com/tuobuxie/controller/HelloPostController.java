267e25a8013474ad10fd9d876e54a7cd2209dab9
lishaofeng@jlpay.com <lishaofeng@jlpay.com>
2019-03-13 15:27:07 +0800

    删除无用文件

--- spring-cloud-zk-root/spring-cloud-zk-producer/src/main/java/com/tuobuxie/controller/HelloPostController.java
+++ spring-cloud-zk-root/spring-cloud-zk-producer/src/main/java/com/tuobuxie/controller/HelloPostController.java
@@ -1,5 +1,6 @@
 package com.tuobuxie.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,13 @@ import java.util.Date;
  * @author: lishaofeng
  **/
 @RestController
+@Slf4j
 public class HelloPostController {
 
 	@PostMapping("/hellopost")
 	public String hello(@RequestBody  String name) {
 		System.out.println("hellopost");
-
+		log.info("hellopost");
 		return "Hello, " + name + " " + new Date();
 	}
 
